b8a19427a8ea0bcac41ec1e64ac715b3c9fe01a1
Alessandro Marega <ale_marega@hotmail.it>
2021-03-02 15:33:36 +0100

    aggiunto programma che stampi i numeri <= 100 e Fizz per multipli di 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // genero i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
+  // per i multipli di 3 stampo Fizz
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
 
-// per i multipli di 3 stampo Fizz
+
 
 // per i multipli di 5 stampo Buzz
 
