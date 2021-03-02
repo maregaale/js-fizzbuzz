c150b1b7e06cf5b7131df6b6563ff58bfc4fb5aa
Alessandro Marega <ale_marega@hotmail.it>
2021-03-02 15:35:21 +0100

    aggiunta stampa di Buzz per multipli di 5

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ for (var i = 1; i <= 100; i++) {
   // per i multipli di 3 stampo Fizz
   if (i % 3 == 0) {
     console.log("Fizz");
-  } else {
+  }
+  // per i multipli di 5 stampo Buzz
+  else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+
+  else  {
     console.log(i);
   }
 }
 
 
 
-// per i multipli di 5 stampo Buzz
+
 
 // per i multipli di 3 e 5 stampo FizzBuzz
