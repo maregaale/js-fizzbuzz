2554c1ba66086ba724e892e338c715d72c47d71c
Alessandro Marega <ale_marega@hotmail.it>
2021-03-02 15:43:30 +0100

    aggiunta stampa FizzBuzz per multipli di 3 e di 5

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
 // genero i numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
+  // per i multipli di 3 e 5 stampo FizzBuzz
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
   // per i multipli di 3 stampo Fizz
-  if (i % 3 == 0) {
+  else if (i % 3 == 0) {
     console.log("Fizz");
   }
   // per i multipli di 5 stampo Buzz
   else if (i % 5 == 0) {
     console.log("Buzz");
   }
-
+  // stampa degli altri numeri
   else  {
     console.log(i);
   }
 }
-
-
-
-
-
-// per i multipli di 3 e 5 stampo FizzBuzz
